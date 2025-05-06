f52d3ac9378c6f8b820172d53343fc3528c0639f
georgetseradze <georgetseradze@gmail.com>
2025-05-06 20:11:47 +0400

    feat(header): finalize header with conditional rendering, dynamic styles and icons

--- apps/web/src/components/layout/header/Header.tsx
+++ apps/web/src/components/layout/header/Header.tsx
@@ -1,39 +1,103 @@
+'use client';
+
 import BurgerIcon from '@/components/icons/BurgerIcon';
 import LogoIcon from '@/components/icons/LogoIcon';
 import ProfileIcon from '@/components/icons/ProfileIcon';
 import SearchIcon from '@/components/icons/SearchIcon';
+import RingIcon from '@/components/icons/RingIcon';
+import { Typography } from '@/components/ui/typography';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import Image from 'next/image';
+import ArrowDownIcon from '@/components/icons/ArrowDownIcon';
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const isBlueBg = pathname === '/' || pathname.startsWith('/tax-returns');
+  const isInfoPage = pathname.startsWith('/information');
+  const isTaxFormPage = pathname.startsWith('/tax-form');
+
   return (
-    <header className="w-full border-b bg-white">
-      <div className="flex items-center justify-between px-4 py-3">
-        <div className="flex items-center gap-6">
-          <Link href="/" className="text-xl font-bold text-[#2b2b2b]">
+    <header
+      className={clsx('w-full', isBlueBg ? 'bg-primary-blue-100' : 'bg-white')}
+    >
+      <div className="relative flex items-center justify-between px-12 py-8">
+        <div className="flex items-center gap-16">
+          <Link href="/">
             <LogoIcon />
           </Link>
+          {isTaxFormPage && (
+            <div>
+              <Typography
+                variant="medium"
+                className="text-primary-dark-400 font-semibold"
+              >
+                Skatturinn
+              </Typography>
+              <Typography className="text-primary-dark-400 font-light text-nowrap">
+                Skattframtal 2024
+              </Typography>
+            </div>
+          )}
         </div>
+        {isTaxFormPage && (
+          <div className="absolute w-[1px] left-60 top-0 bottom-0 bg-secondary-purple-100"></div>
+        )}
 
-        <div className="flex relative items-center">
-          <div className="flex items-center relative w-full max-w-sm">
-            <input
-              type="text"
-              placeholder="Leitaðu á Ísland.is"
-              className="w-full pointer-events-none rounded-md bg-[#F6F9FC] px-4 py-2 pr-10 text-sm placeholder:text-[#1B1B1B] border border-[#D0E3FC] focus:outline-none focus:ring-2 focus:ring-[#0061FF]"
-            />
-            <span className="absolute right-3 top-1/2 -translate-y-1/2 text-[#0061FF]">
+        <div className="flex relative items-center justify-end gap-4 w-full">
+          {isInfoPage && (
+            <div className="flex h-12 items-center justify-between relative gap-8 flex-grow min-w-[200px] max-w-80 rounded-lg bg-[#F6F9FC] px-4 py-[10px] border border-primary-blue-200">
+              <Typography className="text-primary-dark-400 font-normal">
+                Leitaðu á Ísland.is
+              </Typography>
               <SearchIcon className="shrink-0" />
-            </span>
-          </div>
+            </div>
+          )}
 
-          <button className="flex items-center border px-3 py-1 rounded text-sm gap-2 whitespace-nowrap">
-            Mínar síður
-            <ProfileIcon className="shrink-0" />
+          {isInfoPage && (
+            <button className="flex items-center border px-4 py-3 rounded-lg gap-2 whitespace-nowrap border-primary-blue-200">
+              <Typography variant="medium" className="font-semibold">
+                Mínar síður
+              </Typography>
+              <ProfileIcon className="shrink-0" />
+            </button>
+          )}
+          <button className="border px-4 py-3 rounded-lg border-primary-blue-200 bg-white">
+            <Typography variant="medium" className="font-semibold">
+              EN
+            </Typography>
           </button>
-          <button className="border px-3 py-1 rounded text-sm">EN</button>
-          <button className="flex items-center gap-2 border px-3 py-1 rounded text-sm">
-            Valmynd <BurgerIcon />
-          </button>
+
+          {!isInfoPage && (
+            <div className="flex items-center gap-2 px-4 py-3 border border-primary-blue-200 bg-white rounded-lg">
+              <Image
+                src="/avatar.png"
+                alt="Arrow icon"
+                width={24}
+                height={24}
+              />
+              <Typography variant="medium" className="font-semibold">
+                Jökull Þórðarson
+              </Typography>
+              <ArrowDownIcon />
+            </div>
+          )}
+
+          {isInfoPage && (
+            <button className="flex items-center gap-2 px-4 py-3 border border-primary-blue-200 rounded-lg">
+              <Typography variant="medium" className="font-semibold">
+                Valmynd
+              </Typography>
+              <BurgerIcon className="mt-0.5" />
+            </button>
+          )}
+          {!isInfoPage && (
+            <div className="bg-white p-4 border border-primary-blue-200 rounded-lg">
+              <RingIcon />
+            </div>
+          )}
         </div>
       </div>
     </header>
