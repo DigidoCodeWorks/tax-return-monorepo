29d45a78caec2699ed3a691ab0175ddb364ad6ce
georgetseradze <georgetseradze@gmail.com>
2025-05-07 17:11:34 +0400

    wip(graphql): test user and tax return fetch with logging

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,4 @@
-// import { fetchTaxReturnById } from '@/lib/actions/fetchTaxReturn';
+import { fetchTaxReturnById } from '@/lib/actions/fetchTaxReturn';
 import { fetchTaxReturnsByUserId } from '@/lib/actions/fetchTaxReturns';
 import { fetchUserByPhone } from '@/lib/actions/FetchUserByPhone';
 
@@ -6,9 +6,11 @@ export default async function Index() {
   // const taxReturns = await fetchTaxReturnsByUserId('7728391');
   const user = await fetchUserByPhone('7728391');
   const taxReturns = await fetchTaxReturnsByUserId(user.id);
-
-  console.log(taxReturns, 'taxreturns'); // 👈 server-side log
-  // console.log(taxReturns, 'taxreturnssssss'); // 👈 server-side log
+  const taxReturn = await fetchTaxReturnById(
+    taxReturns[taxReturns.length - 1].id,
+  );
+  console.log(taxReturns, 'taxreturnssssss'); // 👈 server-side log
+  console.log(taxReturn, 'taxreturn'); // 👈 server-side log
   console.log(user, 'userr'); // 👈 server-side log
 
   return (
