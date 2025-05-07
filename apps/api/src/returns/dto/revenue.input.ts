0d0ab6553265c08677f02ecf18b7de1c9c6b6449
Imna29 <gio.imnaishvili029@gmail.com>
2025-05-08 01:42:01 +0400

    fix(api): validate tax return id

--- apps/api/src/returns/dto/revenue.input.ts
+++ apps/api/src/returns/dto/revenue.input.ts
@@ -2,10 +2,13 @@ import { InputType, Field } from "@nestjs/graphql";
 import { PensionPaymentInput } from "./pension-payment.input";
 import { VehicleAllowanceInput } from "./vehicle-allowance.input";
 import { WageIncomeInput } from "./wage-income.input";
+import { IsOptional, IsUUID } from "class-validator";
 
 @InputType()
 export class RevenueInput {
   @Field({ nullable: true })
+  @IsOptional()
+  @IsUUID("4")
   taxReturnId?: string;
 
   @Field(() => [WageIncomeInput])
