eeb9689316e6db0c046a42f9d2c2dbccdcf462e9
GeorgeMarych <giorgijirkvelishvili@gmail.com>
2025-05-08 01:31:03 +0400

    feat: develop information page layout and content structure for skattframtal

--- apps/web/src/components/layout/TaxReturnsSidebarDesktop.tsx
+++ apps/web/src/components/layout/TaxReturnsSidebarDesktop.tsx
@@ -3,7 +3,7 @@ import { Typography } from '../ui/typography';
 
 export function TaxReturnsSidebarDesktop() {
   return (
-    <div className="hidden md:block flex flex-col gap-y-6 max-w-[320px] py-6 bg-primary-blue-100 rounded-lg">
+    <div className="hidden md:flex flex-col gap-y-6 max-w-[320px] py-6 bg-primary-blue-100 rounded-lg">
       {' '}
       <Typography variant="h4" as="h4" className="text-primary-blue-600 px-8">
         Netframtal
