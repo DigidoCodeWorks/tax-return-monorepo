cbca0ac8e5d076ee2196b20d356379ff9f7a2aac
georgetseradze <georgetseradze@gmail.com>
2025-05-08 11:04:04 +0400

    feat(auth): Implement login with cookie-based userId storage

--- apps/web/src/lib/actions/loginWithHardcodedPhone.ts
+++ apps/web/src/lib/actions/loginWithHardcodedPhone.ts
@@ -1,8 +1,19 @@
-// app/actions/loginWithHardcodedPhone.ts
 'use server';
 
 import { fetchUserByPhone } from '@/lib/actions/FetchUserByPhone';
+import { cookies } from 'next/headers';
 
 export async function loginWithHardcodedPhone() {
-  return await fetchUserByPhone('7728391');
+  const user = await fetchUserByPhone('7728391');
+  const cookieStore = await cookies();
+  if (user?.id) {
+    cookieStore.set('userId', user.id, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    });
+  }
+
+  return user;
 }
