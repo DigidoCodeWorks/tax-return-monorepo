802c9a7701c07e081a3fe8f2ffb6fe8edc3a42a7
georgetseradze <georgetseradze@gmail.com>
2025-05-08 15:22:17 +0400

    chore(testing): configure and initialize Jest and Playwright

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,10 +1,21 @@
 export default {
   displayName: '@tax-return-app/web',
   preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['<rootDir>/specs/**/*.(spec|test).[tj]s?(x)'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: 'test-output/jest/coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/*.d.ts', '!**/index.ts'],
+  coverageReporters: ['text', 'html', 'lcov'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(graphql-request|@graphql-typed-document-node)/)',
+  ],
 };
